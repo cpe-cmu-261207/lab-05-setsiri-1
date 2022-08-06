5cc89a13f029ad0aff5ba270b28704169c047440
setsiri <76035222+setsiri@users.noreply.github.com>
2022-08-06 07:29:38 +0700

    push

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
 
   //your code here
+  if (inputAdd.value == "") {
+    alert("Todo can not be empty");
+    return;
+  } else {
+    let checkComp = false;
+    addTodo(inputAdd.value, checkComp);
+    inputAdd.value = "";
+    saveTodo;
+  }
 };
 
 function addTodo(title, completed) {
@@ -31,18 +40,65 @@ function addTodo(title, completed) {
   //your code here
   //append todo to HTML...
   //define buttons event...
+
+  div.append(span);
+  div.append(doneBtn);
+  div.append(deleteBtn);
+  todoCtn.prepend(div);
+
+  //mouseover or out
+  doneBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+
+  div.onmouseover = () => {
+    doneBtn.style.display = "";
+    deleteBtn.style.display = "";
+  };
+  div.onmouseout = () => {
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+  };
+
+  let A = 0;
+  doneBtn.onclick = () => {
+    A++;
+    completed = A % 2;
+    span.style.textDecoration = completed ? "line-through" : "";
+    saveTodo();
+  };
+
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+    saveTodo();
+  };
+
+  saveTodo();
 }
 
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
     //your code here
+    const todoObj = {};
+    todoObj.title = todoDiv.children[0].innerText;
+    todoObj.completed =
+      todoDiv.children[0].style.textDecoration === "line-through";
+    data.push(todoObj);
   }
   //your code here
+  data.reverse();
+  const dataStr = JSON.stringify(data);
+  localStorage.setItem("data", dataStr);
 }
 
 function loadTodo() {
   //your code here
+  const dataStr = localStorage.getItem("data");
+  const data = JSON.parse(dataStr);
+
+  for (const todoObj of data) {
+    addTodo(todoObj.title, todoObj.completed);
+  }
 }
 
 loadTodo();
